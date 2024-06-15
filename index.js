fbe1bce16718fccf99be7dc1db3410b89bff8130
Nihal-Sarthi <87559764+Nihal-Sarthi@users.noreply.github.com>
2024-06-14 23:35:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let names = ["Nihal", "Ankit", "Prajjwal", "Pratiksha", "Shriya"];
 function whoIsBuying(names) {
   let numOfPeople = names.length;
-  let indexOfPerson = Math.floor(Math.random() * 5);
+  let indexOfPerson = Math.floor(Math.random() * numOfPeople);
   personBuyingLunch = names[indexOfPerson];
-  return personBuyingLunch;
+  return `${personBuyingLunch} will buy lunch today!`;
 }
